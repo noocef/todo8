863e90c7ee17b15372126918dc33f69be481a910
noocef <moncefazaz@gmail.com>
2021-04-07 13:37:45 +0000

    third commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <div id="app">
             <app></app>
         </div>
-        <script src="{{mix('js/app.js')}">
+        <script src="{{mix('js/app.js')}}">
         </script>
         
     </body>
